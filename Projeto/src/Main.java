57bf73d5beb5e4ca1a2cc9ddc79818595be8fc8d
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-07-16 17:46:42 -0300

    Ensinamentos

---
+++ Projeto/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        numeros inteiros: 100, 21254, 41556, 2, 5
+        byte => 8 bits = -128 a 127
+        short => 16 bits = -32.768 a 32.767
+        int => 32 bits = -2.147.483.648 a 2.147.483.647
+        long => 64 bits = -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+        numeros decimais = 1.2, 3.4, 5.6, 7.8, 9.0
+         5.50
+        float => 32 bits = precisão simples
+        double => 64 bits = precisão dupla
+
+        String => representar palavras e frases ""
+        char => representar um unico caractere = a, b, c, d, ''
+
+        boolean => true ou false
+*/
+
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        String str = "Nícolas";
+        boolean bool = false;
+
+        int[] colecaoDeInteiros = {1, 2, 3, 4, 5};
+
+        if (str == "Nícolas") {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+
+        System.out.println(colecaoDeInteiros[0]);
+    }
+}
