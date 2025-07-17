3dfdc94c3d03fe74837d5917930d484cbbd8c478
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-07-16 22:12:52 -0300

    Mudanças

--- projetos/primeiroprograma/src/Main.java
+++ projetos/primeiroprograma/src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Notas
+        double n1 = 7.0;
+        double n2 = 6.5;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Media
+        double media = (n1 + n2) / 2;
+
+        System.out.println("Nota 1: " + n1);
+        System.out.println("Nota 2: " + n2);
+        System.out.println("Média: " + media);
+
+        if (media >= 7) {
+            System.out.println("Você está Aprovado!");
+        } else {
+            System.out.println("Você está Reprovado!");
         }
+
     }
 }
