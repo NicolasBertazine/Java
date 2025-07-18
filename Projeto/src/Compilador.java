b2dc6f67e8e01049d7ee725ca9b71083106740df
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-07-18 17:49:48 -0300

    Classes, Objetos e Métodos Construtores

--- Projeto/src/Compilador.java
+++ Projeto/src/Compilador.java
@@ -2,6 +2,31 @@ import java.util.ArrayList;
 
 public class Compilador {
     public static void main (String[] args) {
-        
+        Carro meuCarro = new Carro("Fusca");
+        Carro meuCarro1 = new Carro("Jetta");
+        Carro meuCarro2 = new Carro("Onix");
+        Carro meuCarro3 = new Carro("Virtus");
+
+        String result = meuCarro1.acelerar();
+        meuCarro.acelerar();
+        meuCarro1.acelerar();
+        meuCarro2.acelerar();
+        meuCarro3.acelerar();
+
+        System.out.println(result);
     }
+}
+
+class Carro {
+    String modelo;
+
+    public Carro(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String acelerar() {
+        System.out.println("Acelerando o carro " + this.modelo);
+        return "oi";
+    }
+
 }
