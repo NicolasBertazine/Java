4a582e719e6916544590a936a5d3acab4389deb6
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-07-22 17:57:51 -0300

    Aula04

---
+++ Aulas/Aula04/src/aula04/Main.java
@@ -0,0 +1,12 @@
+package aula04;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Nic", "Amarelo", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("Bic", "Azul", 0.5f);
+        c2.status();
+    }
+
+}
